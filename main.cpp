73564937205d32e79e40e0d9c3da69c3b00633ee
Saidou <57505334+souaremsaidou@users.noreply.github.com>
2020-09-11 16:57:19 +0200

    Copy elision
    copy elision refers to a compiler optimization technique that eliminates unnecessary copying of objects

--- main.cpp
+++ main.cpp
@@ -10,28 +10,29 @@ public:
 
     UDT(const UDT &)
     {
+        // the copy constructor has a visible side effect (printing text)
         std::cout << "user-defined copy ctor\n";
     }
 
-    UDT(UDT &&o)
-    noexcept
-    {
-        std::cout << "user-defined move ctor\n";
-    }
+    // UDT(UDT &&o)
+    // noexcept
+    // {
+    //     std::cout << "user-defined move ctor\n";
+    // }
 
     // user-defined copy assignment, copy-and-swap form
-    UDT &operator=(UDT &other)
-    {
-        std::cout << "copy assignment of UDT\n";
-        return *this;
-    }
+    // UDT &operator=(UDT &other)
+    // {
+    //     std::cout << "copy assignment of UDT\n";
+    //     return *this;
+    // }
 
     // user-defined move assignment
-    UDT &operator=(UDT &&other)
-    {
-        std::cout << "move assignment of UDT\n";
-        return *this;
-    }
+    // UDT &operator=(UDT &&other)
+    // {
+    //     std::cout << "move assignment of UDT\n";
+    //     return *this;
+    // }
 
     // destructor prevents implicit move assignment
     ~UDT();
@@ -44,24 +45,48 @@ UDT::UDT(/* args */)
 
 UDT::~UDT()
 {
-    std::cout << "-- destructor\n";
+    std::cout << "user-defined destructor\n";
 }
 
 UDT foo()
 {
     std::cout << "foo\n";
-    // UDT local_udt;
+    // UDT local_udt; // direct-initialization, calls UDT::UDT()
     // return local_udt;
     return UDT();
 }
 
+//----------------------------------------------
+// copy elision
+//----------------------------------------------
+//
+// Omits copy and move (since C++11) constructors,
+// resulting in zero-copy pass-by-value semantics.
+void copy_elision()
+{
+    UDT udt(foo());
+}
+
+//----------------------------------------------
+// RVO
+//----------------------------------------------
+// C++ standard allows an implementation to omit creating
+// - a temporary used to initialize another object of the same type.
+//
+// GCC compiler support
+// Specifying -fno-elide-constructors option disables that optimization,
+// and forces G++ to call the copy constructor in all cases.
+void rvo()
+{
+    UDT obj = foo();
+}
+
 int main(int, char **)
 {
-    //the compiler to create temporary objects
+    // the compiler to create temporary objects
     // foo();
 
-    // copy elision
-    // Omits copy and move (since C++11) constructors,
-    // resulting in zero-copy pass-by-value semantics.
-    UDT udt(foo());
+    // rvo();
+
+    copy_elision();
 }
